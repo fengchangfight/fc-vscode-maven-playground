e8b80d383e0073dd4ef0c581e18a9168d5b9d8c4
fengchang <fengchang_fight@163.com>
2019-09-29 06:38:50 +0800

    add computeIfAbsent

--- src/main/java/com/fcvscodemvn/datastructure/HashmapDemo.java
+++ src/main/java/com/fcvscodemvn/datastructure/HashmapDemo.java
@@ -1,16 +1,35 @@
 package com.fcvscodemvn.datastructure;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HashmapDemo {
-    public static void main(String[] args) {
+    public static void maptrick() {
+        Map<String, List<String>> complexMap = new HashMap<>();
+
+        List<String> kkval = complexMap.computeIfAbsent("kk", rk -> new ArrayList());
+
+        kkval.add("hell");
+
+        System.out.println(kkval);
+    }
+
+    public static void initMap() {
         Map<String, Object> mp = new HashMap<String, Object>() {
             {
                 put("text", "Time out");
                 put("expect_user_talk", true);
             }
         };
+
         System.out.println(mp);
+    }
+
+    public static void main(String[] args) {
+        initMap();
+        System.out.println("==========");
+        maptrick();
     }
 }
