9667cfaf6df6100117a8fd832c8e8706a0909548
fengchang <fengchang_fight@163.com>
2019-12-08 20:50:56 +0800

    neat code for log

--- src/main/java/com/fcvscodemvn/logging/Log4jExample.java
+++ src/main/java/com/fcvscodemvn/logging/Log4jExample.java
@@ -3,17 +3,19 @@ package com.fcvscodemvn.logging;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.*;
 import java.sql.SQLException;
 
+@Slf4j
 public class Log4jExample {
 
-    /* Get actual class name to be printed on */
-    static Logger log = Logger.getLogger(Log4jExample.class.getName());
+    // /* Get actual class name to be printed on */
+    // static Logger log = Logger.getLogger(Log4jExample.class.getName());
 
     public static void main(String[] args) throws IOException, SQLException {
-
-        log.debug("Hello this is a debug message");
-        log.info("Hello this is an info message");
+        log.debug("Hello this is a debug message: " + System.currentTimeMillis());
+        log.info("Hello this is an info message: " + System.currentTimeMillis());
     }
 }
